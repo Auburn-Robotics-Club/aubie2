96036a4fc6b7c2a9c6d01b99e568ae2adcbeff7c
Tropical <42101043+Tropix126@users.noreply.github.com>
2025-01-23 19:19:34 -0600

    adjust red_left

--- packages/hailstorm/src/autons/red_left.rs
+++ packages/hailstorm/src/autons/red_left.rs
@@ -1,7 +1,6 @@
 use alloc::boxed::Box;
 use core::{error::Error, f64::consts::PI, time::Duration};
 
-use aubie2::subsystems::{intake::RejectColor, lady_brown::LadyBrownTarget};
 use evian::{
     differential::motion::{BasicMotion, Seeking},
     prelude::*,
@@ -52,7 +51,8 @@ pub async fn red_left(bot: &mut Robot) -> Result<(), Box<dyn Error>> {
     basic.drive_distance(dt, 24.0).await;
     _ = bot.doinker.set_low();
 
-    basic.turn_to_heading(dt, 324.0.deg()).await;
+    // Clamp goal
+    basic.turn_to_heading(dt, 318.0.deg()).await;
     basic
         .linear_controller
         .set_output_limit(Some(Motor::V5_MAX_VOLTAGE * 0.35));
@@ -65,58 +65,80 @@ pub async fn red_left(bot: &mut Robot) -> Result<(), Box<dyn Error>> {
     _ = bot.clamp.set_high();
     sleep(Duration::from_millis(250)).await;
 
+    // Score first ring
     bot.lift.set_target(Position::from_degrees(300.0));
     sleep(Duration::from_millis(800)).await;
     bot.lift.set_target(Position::from_degrees(70.0));
 
-    basic.turn_to_heading(dt, 240.0.deg()).await;
+    // Intake stack
+    basic.turn_to_heading(dt, 243.0.deg()).await;
     _ = bot.clamp.set_low();
     basic
         .linear_controller
         .set_output_limit(Some(Motor::V5_MAX_VOLTAGE * 0.35));
     basic.drive_distance(dt, 23.0).await;
+    sleep(Duration::from_millis(1250)).await;
+
+    // Get second goal
+    basic.turn_to_heading(dt, 183.0.deg()).await;
+
+    basic
+        .linear_controller
+        .set_output_limit(Some(Motor::V5_MAX_VOLTAGE * 0.4));
+    basic.drive_distance_at_heading(dt, -44.0, 181.0.deg()).await;
     basic
         .linear_controller
         .set_output_limit(Some(Motor::V5_MAX_VOLTAGE));
-    sleep(Duration::from_millis(1250)).await;
 
-    basic.turn_to_heading(dt, 180.0.deg()).await;
-    basic.drive_distance(dt, -38.0).await;
     _ = bot.clamp.set_high();
     sleep(Duration::from_millis(250)).await;
 
-    bot.lift.set_target(Position::from_degrees(300.0));
+    bot.lift.set_target(Position::from_degrees(285.0));
     sleep(Duration::from_millis(800)).await;
     bot.lift.set_target(Position::from_degrees(70.0));
+    sleep(Duration::from_millis(500)).await;
 
-    basic.drive_distance(dt, -20.0).await;
+    basic.drive_distance_at_heading(dt, -14.0, 180.0.deg()).await;
     _ = bot.clamp.set_low();
 
-    basic.turn_to_heading(dt, 214.0.deg()).await;
-    basic.drive_distance(dt, 16.0).await;
+    // Score on alliance stake
+    basic.drive_distance_at_heading(dt, 14.0, 180.0.deg()).await;
 
-    basic.turn_to_heading(dt, 180.0.deg()).await;
-    sleep(Duration::from_millis(250)).await;
-    basic.drive_distance(dt, 5.0).await;
-    sleep(Duration::from_millis(250)).await;
+    basic.turn_to_heading(dt, 270.0.deg()).await;
+
+    basic
+        .linear_controller
+        .set_output_limit(Some(Motor::V5_MAX_VOLTAGE * 0.15));
+    basic.drive_distance_at_heading(dt, 17.5, 270.0.deg()).await;
+    basic
+        .linear_controller
+        .set_output_limit(Some(Motor::V5_MAX_VOLTAGE));
+
+    basic.drive_distance_at_heading(dt, -8.0, 270.0.deg()).await;
 
     basic.turn_to_heading(dt, 90.0.deg()).await;
     sleep(Duration::from_millis(250)).await;
-    basic.drive_distance(dt, -4.0).await;
+    basic.drive_distance_at_heading(dt, -7.5, 90.0.deg()).await;
     sleep(Duration::from_millis(250)).await;
 
-    bot.lift.set_target(Position::from_degrees(300.0));
-    sleep(Duration::from_millis(800)).await;
+    bot.lift.set_target(Position::from_degrees(285.0));
+    sleep(Duration::from_secs(1)).await;
     bot.lift.set_target(Position::from_degrees(70.0));
+    sleep(Duration::from_millis(500)).await;
 
     for motor in bot.intake.iter_mut() {
         _ = motor.set_voltage(0.0);
     }
 
-    seeking.move_to_point(dt, (0.0, 15.0)).await;
+    seeking.move_to_point(dt, (0.0, 13.0)).await;
     basic.turn_to_heading(dt, 225.0.deg()).await;
+    
+    _ = bot.lift.raise();
+    sleep(Duration::from_millis(800)).await;
+    bot.lift.set_target(Position::from_degrees(310.0));
 
-    seeking.move_to_point(dt, (11.0, 23.0)).await;
+    basic.drive_distance(dt, -7.0).await;
+    _ = bot.lift.lower();
 
     Ok(())
 }
